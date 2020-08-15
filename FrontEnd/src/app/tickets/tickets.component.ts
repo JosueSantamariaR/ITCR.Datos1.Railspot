2e4fe87f940e4aaab07fef093cbc280d1392ad5c
Josué Santamaría <gabosr16@estudiantec.cr>
2020-08-15 13:31:00 -0600

    Update

--- FrontEnd/src/app/tickets/tickets.component.ts
+++ FrontEnd/src/app/tickets/tickets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,9 +10,14 @@ export class TicketsComponent {
   userID: number;
   userdescount: number;
   descount: JSON;
-  
+  request: JSON;
+  startB:string;
+  endB: string;
   start:string;
+  date:Date;
+  routeB= "Moravia";
   end:string;
+  success="";
   total=0;
   desc=2;
 
@@ -23,16 +28,36 @@ export class TicketsComponent {
     let price=0;
     this.httpClient.get('http://localhost:5000//dijkstra/',{params:{"start":this.start, "end": this.end}}).subscribe(
        (data) => {
-         this.descount = data as JSON;
-         this.total=0;
-         price =0;
-         for (let i=0; i<this.userdescount;i++){
-           price = ((this.descount["cost"]*25)*((this.desc/100)));
-           this.total+=((this.descount["cost"]*25)-price);
-           if(this.desc<=90){
-             this.desc+=2;}
-           }
-        
-  });   
+        this.descount = data as JSON;
+        this.total=0;
+        price =0;
+        for (let i=0; i<this.userdescount;i++){
+          price = ((this.descount["cost"]*25)*((this.desc/100)));
+          this.total+=((this.descount["cost"]*25)-price);
+          if(this.desc<=90){
+            this.desc+=2;}
+          }
+    }); 
+    console.log(this.userdescount);
+
+
   }
+  buyTickets(){
+    for (let i=0; i<this.userdescount;i++
+      ){
+    if(this.userID != null 
+      && this.start != null 
+      && this.end != null && 
+      this.date != null)
+      { 
+      this.httpClient.post(`http://localhost:5000/saveUserTicket/${this.userID},${this.start},${this.end},${this.date}`,{}).subscribe((data)=>{
+        this.request=data as JSON;
+        this.date=null;
+        this.success="Exitoso";
+        console.log(this.date);
+      });
+  }}
+
+  
+}
 }
